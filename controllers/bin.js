52b95485b599c432e15cb5b746fede4670f8da41
Sujin Kang <1214sujin@naver.com>
2023-12-06 21:10:58 +0900

    쓰레기통 관리 기능 (back): result반환 및 sql escape 수정/추가

--- controllers/bin.js
+++ controllers/bin.js
@@ -17,10 +17,10 @@ module.exports = {
 	},
 	list: (req, res) => {
 		var { dong_id } = req.params
-		var sql0 = `select bin_id from bin where pos=${dong_id};`
-		db.query(sql0, (err, results) => {
+		var sql0 = `select bin_id from bin where pos=?;`
+		db.query(sql0, [dong_id], (err, result) => {
 			var data = {
-				"bin_id": results
+				'bin_id': result
 			}
 			res.json(data)
 		})
@@ -29,10 +29,10 @@ module.exports = {
 		var { bin_id } = req.params
 		var sql0 = `select * from pos_gu;`
 		var sql1 = `select * from pos_dong;`
-		var sql2 = `select * from bin inner join pos_dong on dong_id=pos\
-					join pos_gu on gu_id=p_gu where bin_id="${bin_id}";`
-		var sql3 = `select name from bin b inner join employee e on e.pos=b.pos where bin_id="${bin_id}"`
-		db.query(sql0+sql1+sql2+sql3, (err, results) => {
+		var sql2 = `select * from bin inner join pos_dong on dong_id=pos
+					join pos_gu on gu_id=p_gu where bin_id=?;`
+		var sql3 = `select name from bin b inner join employee e on e.pos=b.pos where bin_id=?;`
+		db.query(sql0+sql1+sql2+sql3, [bin_id, bin_id], (err, results) => {
 			var data = {
 				'gu_list': results[0],
 				'dong_list': results[1],
@@ -56,31 +56,34 @@ module.exports = {
 	create: (req, res) => {
 		var { bin_id, dong_id, lat, lon } = req.body
 		console.log(bin_id, dong_id, lat, lon)
-		db.query(`insert into bin (bin_id, pos, lat, lon, install_date) values (?, ?, ?, ?, date_format(now(), '%Y-%m-%d'));`,[bin_id, dong_id, lat, lon], (err, result) =>{
-			if (err) {
-				if (err.code == 'ER_BAD_NULL_ERROR') res.json({'result': '비어있는 값이 존재합니다.'})
-				else if (err.code == 'ER_DUP_ENTRY') res.json({'result': '중복된 쓰레기통 ID입니다.'})
-				else res.json({'result': '신규 쓰레기통 정보 등록에 실패했습니다.'})
+		var sql0 = `insert into bin (bin_id, pos, lat, lon, install_date) values (?, ?, ?, ?, date_format(now(), '%Y-%m-%d'));`
+		db.query(sql0, [bin_id, dong_id, lat, lon], (err, result) =>{
+			if (err) { console.log(err)
+				if (err.code == 'ER_BAD_NULL_ERROR') res.json({'err': '비어있는 값이 존재합니다.'})
+				else if (err.code == 'ER_DUP_ENTRY') res.json({'err': '중복된 쓰레기통 ID입니다.'})
+				else res.json({'err': err.errno})
 			}
-			else res.json({'result': 1})
+			else res.json({'err': 0})
 		})
 	},
 	update: (req, res) => {
 		var { bin_id } = req.params
 		var { dong_id, lat, lon } = req.body
-		db.query(`update bin set pos=?, lat=?, lon=? where bin_id=?`, [dong_id, lat, lon, bin_id], (err, result) => {
+		var sql0 = `update bin set pos=?, lat=?, lon=? where bin_id=?;`
+		db.query(sql0, [dong_id, lat, lon, bin_id], (err, result) => {
 			if (err) {
-				if (err.code == 'ER_BAD_NULL_ERROR') res.json({'result': '비어있는 값이 존재합니다.'})
-				else res.json({'result': '쓰레기통 정보 수정에 실패했습니다.'})
+				if (err.code == 'ER_BAD_NULL_ERROR') res.json({'err': '비어있는 값이 존재합니다.'})
+				else res.json({'err': err.errno})
 			}
-			else res.json({'result': 1})
+			else res.json({'err': 0})
 		})
 	},
 	delete: (req, res) => {
 		var { bin_id } = req.params
-		db.query(`delete from bin where bin_id=?`, [bin_id], (err, result) => {
-			if (err) res.json({'result': '쓰레기통 정보 삭제에 실패했습니다.'})
-			else res.json({'result': 1})
+		var sql0 = `delete from bin where bin_id=?;`
+		db.query(sql0, [bin_id], (err, result) => {
+			if (err) res.json({'err': err.errno})
+			else res.json({'err': 0})
 		})
 	},
 }
