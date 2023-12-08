9868df91b4b6fafb55bf3818c1156f6721612c84
Sujin Kang <1214sujin@naver.com>
2023-12-09 00:01:04 +0900

    mobile API (back): err처리 수정, alarm 사진 전송, +replace

--- controllers/mobile.js
+++ controllers/mobile.js
@@ -1,18 +1,24 @@
-db = require('../db/db')
+const fs = require('fs')
+var db = require('../db/db')
 
 module.exports = {
 	home: (req, res) => {
 		var sql0 = `select b.bin_id, lat, lon, amount from bin b left join ( select * from loadage where (bin_id, load_time)
 						in (select bin_id, max(load_time) from loadage group by bin_id) ) as l on b.bin_id=l.bin_id;`
 		db.query(sql0, (err, result) => {
-			var data = { 'err': 0, 'bin_list': result }
-			if (err) data['err'] = err.errno
-			res.json(data)
+			if (err) {
+				console.log(err)
+				res.json({ 'err': err.errno })
+			} else {
+				var data = { 'err': 0, 'bin_list': result }
+				res.json(data)
+			}
 		})
 	},
 	list: (req, res) => {
-		var sql0 = `select b.bin_id, gu, dong, lat, lon, install_date, amount, load_time, rp_time, emp_id, fire_time from bin b
-					join pos_dong on pos=dong_id join pos_gu on p_gu=gu_id
+		var sql0 = `select b.bin_id, gu, dong, lat, lon, date_format(install_date, '%Y년 %m월 %d일') as install_date,
+						amount, date_format(load_time, '%H:%i:%s') as load_time, rp_time, emp_id, date_format(fire_time, '%H:%i:%s') as fire_time
+					from bin b join pos_dong on pos=dong_id join pos_gu on p_gu=gu_id
 					left join ( select * from loadage where (bin_id, load_time)
 						in (select bin_id, max(load_time) from loadage group by bin_id) ) as l on b.bin_id=l.bin_id
 					left join ( select * from replacement where (bin_id, rp_time)
@@ -20,24 +26,58 @@ module.exports = {
 					left join ( select * from fire where (bin_id, fire_time)
 						in (select bin_id, max(fire_time) from fire group by bin_id) ) as f on b.bin_id=f.bin_id;`
 		db.query(sql0, (err, result) => {
-			var data = { 'err': 0, 'bin_list': result }
-			if (err) data['err'] = err.errno
-			res.json(data)
+			if (err) {
+				console.log(err)
+				res.json({ 'err': err.errno })
+			} else {
+				var data = { 'err': 0, 'bin_list': result }
+				res.json(data)
+			}
 		})
 	},
 	alarm: (req, res) => {
-		var sql0 = `select bin_id, amount, load_time from loadage where (bin_id, load_time)
-					in (select bin_id, max(load_time) from loadage group by bin_id) and amount>=90;`
-		var sql1 = `select bin_id, fire_time from fire where (bin_id, fire_time)
-					in (select bin_id, max(fire_time) from fire group by bin_id);`
+		var sql0 = `select l.bin_id, gu, dong, amount, l.load_time, load_img from loadage l
+					join bin b on b.bin_id=l.bin_id join pos_dong on pos=dong_id join pos_gu on p_gu=gu_id join notified n on l.bin_id=n.bin_id and l.load_time=n.load_time
+					where (l.bin_id, l.load_time) in (select bin_id, max(load_time) from loadage group by bin_id) and amount>=90;`
+		var sql1 = `select f.bin_id, gu, dong, fire_time, fire_img from fire f
+					join bin b on b.bin_id=f.bin_id join pos_dong on pos=dong_id join pos_gu on p_gu=gu_id
+					where (f.bin_id, fire_time) in (select bin_id, max(fire_time) from fire group by bin_id);`
 		db.query(sql0+sql1, (err, results) => {
-			var data = {
-				'err': 0,
-				'load_list': results[0],
-				'fire_list': results[1]
+			if (err) {
+				console.log(err)
+				res.json({ 'err': err.errno })
+			} else {
+				var data = {
+					'err': 0,
+					'load_list': results[0],
+					'fire_list': results[1],
+				}
+				for (let i=0; i<results[0].length; i++) {
+					if (results[0][i].load_img) {
+						let image = fs.readFileSync(__dirname+'/../public/'+results[0][i].load_img);
+						data['load_list'][i].load_img = Buffer.from(image).toString('base64')
+					}
+				}
+				for (let i=0; i<results[1].length; i++) {
+					if (results[1][i].fire_img) {
+						let image = fs.readFileSync(__dirname+'/../public/'+results[1][i].fire_img);
+						data['fire_list'][i].fire_img = Buffer.from(image).toString('base64')
+					}
+				}
+				res.json(data)
 			}
-			if (err) data['err'] = err.errno
-			res.json(data)
+		})
+	},
+	replace: (req, res) => {
+		var { emp_id, bin_id } = req.body
+		var sql0 = `insert into replacement values(?, now(), ?)`
+		db.query(sql0, [bin_id, emp_id], (err, result) => {
+			if (err) {
+				console.log(err)
+				res.json({ 'err': err.errno })
+			} else {
+				res.json({ 'err': 0 })
+			}
 		})
 	},
 	// 로그인이나 정보 수정 시 사용자의 정보를 모바일에 전송 (정보 확인 시마다 요청하지 않아도 되도록)
--- routers/mobileRouter.js
+++ routers/mobileRouter.js
@@ -8,6 +8,7 @@ var info = require('../controllers/bin').new
 router.get('/home', mobile.home)
 router.get('/list', mobile.list)
 router.get('/alarm', mobile.alarm)
+router.post('/replacement', mobile.replace)
 // 회원 관련 기능(로그인, )
 router.post('/login', mobile.login)
 router.get('/info', info)
